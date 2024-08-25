6c1ae3a48812d8edee5d5a85a62ea72ed3c49b07
dnsptpv <115485192+dnsptpv@users.noreply.github.com>
2024-08-25 11:46:32 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,6 @@ use bevy::app::App;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
-use bevy_vector_shapes::prelude::*;
 
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
@@ -46,7 +45,7 @@ impl Plugin for GamePlugin {
 
         #[cfg(debug_assertions)]
         {
-            app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default(), Shape2dPlugin::default()));
+            app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()));
         }
     }
 }
