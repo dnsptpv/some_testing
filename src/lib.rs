68e2260e736a1a4f965f9277e03d4c54b1110f84
dnsptpv <115485192+dnsptpv@users.noreply.github.com>
2024-08-25 19:54:21 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@
 
 //mod actions;
 //mod audio;
-//mod loading;
-//mod menu;
+mod loading;
+mod menu;
 //mod player;
 
 //use crate::actions::ActionsPlugin;
 //use crate::audio::InternalAudioPlugin;
-//use crate::loading::LoadingPlugin;
-//use crate::menu::MenuPlugin;
+use crate::loading::LoadingPlugin;
+use crate::menu::MenuPlugin;
 //use crate::player::PlayerPlugin;
 
 use bevy::app::App;
@@ -20,27 +20,27 @@ use bevy::prelude::*;
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
 // Or https://github.com/bevyengine/bevy/blob/main/examples/ecs/state.rs#[derive(States, Default, Clone, Eq, PartialEq, Debug, Hash)]
-//enum GameState {
+enum GameState {
     // During the loading State the LoadingPlugin will load our assets
-   // #[default]
-//    Loading,
+    #[default]
+    Loading,
     // During this State the actual game logic is executed
-//    Playing,
+    Playing,
     // Here the menu is drawn and waiting for player interaction
-//    Menu,
-//}
+    Menu,
+}
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        //app.init_state::<GameState>();//.add_plugins((
-           // LoadingPlugin,
-            //MenuPlugin,
+        app.init_state::<GameState>().add_plugins((
+            LoadingPlugin,
+            MenuPlugin,
            // ActionsPlugin,
            // InternalAudioPlugin,
            // PlayerPlugin,
-        //));
+        ));
 
         #[cfg(debug_assertions)]
         {
